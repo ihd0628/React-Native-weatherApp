20ca438b5a4b15b0bbc96cb07a4c3e63f1dfcee3
Hoon <si932174@gmail.com>
2023-01-25 16:27:40 +0900

    learn : How to Set React Native Layout

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View
+        style={{
+          flex: 3,
+          backgroundColor: "teal",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Hello RN Layout!</Text>
+      </View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
